082b7fb2d54944238c121de32f8963a163d02fca
Klaytn <122271792+AtulAV7@users.noreply.github.com>
2024-09-22 20:00:17 +0530

    Added comments to line 10 and 14

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import qrcode 
+
+
 from PIL import Image
+
+
 import qrcode.constants
+
 qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=15,border=5)
-qr.add_data("https://x.com/home")
+qr.add_data("https://x.com/home")#Give the link that you want to convert to qrcode form within the " "
 qr.make(fit=True)
+
 img=qr.make_image(fill_color="red",back_color="black")
 img.save("Twitter.png")
