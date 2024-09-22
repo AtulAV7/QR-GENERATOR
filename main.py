49f589435d46a3664892af1d9c8348ae77e0f1a3
Klaytn <122271792+AtulAV7@users.noreply.github.com>
2024-09-22 20:12:57 +0530

    2

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from PIL import Image
 import qrcode.constants
 
 qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=15,border=5)
-qr.add_data("https://x.com/home")#Give the link that you want to convert to qrcode form within the " "
+qr.add_data("https://chatgpt.com/")#Give the link that you want to convert to qrcode form within the " "
 qr.make(fit=True)
 
 img=qr.make_image(fill_color="red",back_color="black")
-img.save("Twitter.png")
+img.save("chatgpt.png")
